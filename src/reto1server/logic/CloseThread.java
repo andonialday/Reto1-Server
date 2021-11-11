b9d25b3677deacd7d869c8408e1eaeec8fe9f07d
andonialday <andalday@gmail.com>
2021-11-11 08:47:15 +0100

    Thread to close the Server by a console input of "exit". Ready to merge

--- src/reto1server/logic/CloseThread.java
+++ src/reto1server/logic/CloseThread.java
@@ -10,12 +10,18 @@ import java.util.logging.Logger;
 import reto1server.application.ServerApplication;
 
 /**
+ * Thread designed to close the server from an input ofthe console
  *
- * @author aitor
+ * It runs on Server start, and when an input of "exit" happens on the console,
+ * sends an order close the application andthe connections to the DB
+ *
+ * @author Andoni Alday
  */
 public class CloseThread extends Thread {
 
-    private static final Logger LOGGER = Logger.getLogger("package.class");;
+    private static final Logger LOGGER = Logger.getLogger("package.class");
+
+    ;
     
     @Override
     public void run() {
@@ -26,7 +32,7 @@ public class CloseThread extends Thread {
             if (cl.equalsIgnoreCase("exit")) {
                 LOGGER.info("Server closing request received, closing the server");
                 ServerApplication.finish();
-                
+
             }
         }
     }
